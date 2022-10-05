d40dc68e955df259f6ed9895fbe75f9adfe1f60a
sgujuluva <sanghee04@gmail.com>
2022-10-05 16:10:30 +0200

    ready to deploy

--- routes/RegisterRoutes.js
+++ routes/RegisterRoutes.js
@@ -1,41 +1,39 @@
 import express from "express";
-import User from "../models/RegisterModel.js"
+import User from "../models/RegisterModel.js";
 
 const router = express.Router();
 
 //creating new user
-router.post("/create", async(req,res) => {
-    try{
-        const createdUser = await User.create({
-            username: req.body.username,
-            email:req.body.email,
-            password:req.body.password
-        })
-        return res.status(200).json(createdUser)
-    }catch(error){
-        return res.status(500).json(error.message)
-    }
-   
-})
+router.post("/create", async (req, res) => {
+  try {
+    const createdUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(200).json(createdUser);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
 
 //getting all user
-router.get("/getusers", async(req,res) => {
-    try{
-        const getUsers = await User.find();
-        return res.status(200).json(getUsers)
-    }catch(error){
-        return res.status(500).json(error.message)
-    }
-   
-})
+router.get("/getusers", async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    return res.status(200).send(getUsers);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
 
 //getting user by id
 
-router.get("/users/byid/:id", async(req,res) => {
+// router.get("/users/byid/:id", async(req,res) => {
 
-    const getUserById = await User.findById(req.params.id)
+//     const getUserById = await User.findById(req.params.id)
 
-    return res.status(200).json(getUserById)
-})
+//     return res.status(200).json(getUserById)
+// })
 
-export default router
+export default router;
--- server.js
+++ server.js
@@ -2,17 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/RegisterRoutes.js";
-import orderRoutes from "./routes/OrderRoutes.js"
-
+import orderRoutes from "./routes/OrderRoutes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
+app.use(express());
 
 //connecting to the database
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`)
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  )
   .then(() => {
     console.log("Database connected! 😃");
   })
@@ -22,9 +29,9 @@ mongoose
   });
 
 //registering routes
-app.use("/api/users", userRoutes);
-app.use("/api/orders",orderRoutes)
+app.use("/users", userRoutes);
+app.use("/orders", orderRoutes);
 
 app.listen(process.env.PORT || 6050, () => {
-    console.log(`Server is running on PORT `)
-})
+  console.log(`Server is running on PORT `);
+});
