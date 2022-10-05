5822ed2905eb5098751702ba45eca9445af6983e
sgujuluva <sanghee04@gmail.com>
2022-10-05 11:41:00 +0200

    get orders

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -4,6 +4,15 @@ import Order from "../models/OrderModel.js"
 
 const router = express.Router();
 
+//get all orders
+router.get("/", async(req,res) => {
+
+    const getOrders = await Order.find().populate("user")
+    return res.status(200).json(getOrders)
+})
+
+//get orders by userId
+
 //post
 router.post("/create", async(req,res) => {
     const createOrder = await Order.create({
