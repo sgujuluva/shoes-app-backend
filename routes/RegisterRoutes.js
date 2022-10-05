14e42c1da49a35c3baa052761ba64cb74eebde3b
sgujuluva <sanghee04@gmail.com>
2022-10-05 10:41:58 +0200

    registerroute for post created

--- routes/RegisterRoutes.js
+++ routes/RegisterRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
+import User from "../models/RegisterModel.js"
 
 const router = express.Router();
 
 router.post("/create", async(req,res) => {
     try{
         const createdUser = await User.create({
-            
+            username: req.body.username,
+            email:req.body.email,
+            password:req.body.password
         })
+        return res.status(200).json(createdUser)
+    }catch(error){
+        return res.status(500).json(error.message)
     }
+   
 })
 
 export default router
