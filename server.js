d1a1dbf4526a23c9d33ceb1ecd4c53a3efd58a70
sgujuluva <sanghee04@gmail.com>
2022-10-05 10:23:34 +0200

    project connected to database

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+//connecting to the database
+mongoose
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`)
+  .then(() => {
+    console.log("Database connected! 😃");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    console.log("🤨");
+  });
+
+app.listen(process.env.PORT || 4050, () => {
+    console.log(`Server is running on PORT `)
+})
