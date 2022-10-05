02f9e139a184e604236d094f361e56a65b5d1c44
sgujuluva <sanghee04@gmail.com>
2022-10-05 10:38:16 +0200

    userschema designed

--- models/RegisterModel.js
+++ models/RegisterModel.js
@@ -1,2 +1,13 @@
 import mongoose from "mongoose";
 
+const {Schema,model} = mongoose;
+
+const userSchema = new Schema({
+    username: String,
+    email:String,
+    password:String
+})
+
+const User = model("User", userSchema)
+
+export default User
