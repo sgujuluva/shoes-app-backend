f67a87fb1aa436cbe260defdbc7bd2fa7101b1d6
sgujuluva <sanghee04@gmail.com>
2022-10-05 10:33:42 +0200

    creating schema for user doc

--- routes/RegisterRoutes.js
+++ routes/RegisterRoutes.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-
+router.post("/create", async(req,res) => {
+    try{
+        const createdUser = await User.create({
+            
+        })
+    }
+})
 
 export default router
